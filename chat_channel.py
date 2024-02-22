f5b4d2dfe0d32a19d28f5b65a1eb8e24002460e0
anion <1005128408@qq.com>
2024-02-22 12:22:43 +0800

    2.22.12

--- chat_channel.py
+++ chat_channel.py
@@ -2,12 +2,23 @@ is_quit = 0
 import gradio as gr
 import os
 from openai import OpenAI
+
+
 def stop_generation():
     global is_quit
     is_quit = 1
 
-system_prompt = "请你作为一名任何领域的面试官，面试前来的应聘者。我们的面试采用一问一答的形式，一开始，我将向你说明我前来应聘的职位，而你一开始会回答应聘本职位的一些注意事项，并紧接着进入角色，问出面试的第一个问题。当我说“停止面试”，你会告诉我是否被聘用以及被或不被聘用的原因，并紧接着退出角色，复盘本次面试是否达到要求。你的语气应该是严格和尊重的，但是带一点长辈对晚辈的轻佻和不屑,如果你感觉对方没有诚意，可以责备对方并立即离场，然后退出角色并直接进入复盘。现在，我要告诉你我面试的岗位。"
 
+identity = "请你作为一名任何领域的专业面试官，面试前来的应聘者。"
+flow = "我们的面试采用一问一答的形式，一开始，我将向你说明我前来应聘的职位，而你一开始会回答应聘本职位的一些注意事项，并紧接着进入角色，问出面试的第一个问题。当我说“停止面试”，你会告诉我是否被聘用以及被或不被聘用的原因，并紧接着退出角色，复盘本次面试是否达到要求。"
+character = "进入角色后，你的语气应该立马变得严厉，并且带居高临下的姿态,如果你感觉对方没有诚意，可以简单责备对方并立即离场，然后退出角色并直接复盘本次失败的原因和本次面试的发挥。注意，当你退出角色之前，你的责备应该口语化而不是一大串，应该区分没有“能力”和没有“诚意”"
+if os.getenv("identity") != None:
+    identity = os.getenv("identity")
+if os.getenv("flow") != None:
+    flow = os.getenv("flow")
+if os.getenv("character") != None:
+    character = os.getenv("character")
+system_prompt = identity + flow + character + "现在，我要告诉你我面试的岗位。"
 
 def bot(message, history, model):
     history_openai_format = [ {"role": "system", "content": system_prompt}]
